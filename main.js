664207e67cd4d26ca8c7c3344d183b8c19167b4b
MGibson7 <MGibsonASC@gmail.com>
2022-04-06 15:28:49 -0400

    Updates

--- main.js
+++ main.js
@@ -75,11 +75,21 @@ const footballTexture = new THREE.TextureLoader().load('football.png');
 const etchTexture = new THREE.TextureLoader().load('etch.jpeg');
 const calcTexture = new THREE.TextureLoader().load('calculator.jpeg');
 const invTexture = new THREE.TextureLoader().load('invoice.jpg');
+const dtvTexture = new THREE.TextureLoader().load('dtv.jpg');
+const conTexture = new THREE.TextureLoader().load('contact.png');
+const viaTexture = new THREE.TextureLoader().load('viasat.png');
+const fireTexture = new THREE.TextureLoader().load('fire.png');
+const junkTexture = new THREE.TextureLoader().load('junk.webp');
 const michael2 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: footballTexture }));
 const michael3 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: robotTexture }));
 const michael4 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: etchTexture }));
 const michael5 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: calcTexture }));
 const michael6 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: invTexture }));
+const michael7 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: dtvTexture }));
+const michael8 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: conTexture }));
+const michael9 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: viaTexture }));
+const michael10 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: fireTexture }));
+const michael11 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: junkTexture }));
 
 scene.add(michael);
 scene.add(michael2);
@@ -87,9 +97,14 @@ scene.add(michael3);
 scene.add(michael4);
 scene.add(michael5);
 scene.add(michael6);
+scene.add(michael7);
+scene.add(michael8);
+scene.add(michael9);
+scene.add(michael10);
+scene.add(michael11);
 
 
-let array1 = [michael2, michael3, michael4, michael5, michael6]
+let array1 = [michael2, michael3, michael4, michael5, michael6, michael7, michael8, michael9, michael10, michael11]
 
 
 michael.position.z = 10;
@@ -109,6 +124,21 @@ michael5.position.z = 10;
 michael6.position.y = -10;
 michael6.position.x = 30;
 michael6.position.z = 10;
+michael7.position.y = -10;
+michael7.position.x = 35;
+michael7.position.z = 10;
+michael8.position.y = -10;
+michael8.position.x = 40;
+michael8.position.z = 10;
+michael9.position.y = -10;
+michael9.position.x = 45;
+michael9.position.z = 10;
+michael10.position.y = -10;
+michael10.position.x = 50;
+michael10.position.z = 10;
+michael11.position.y = -10;
+michael11.position.x = 55;
+michael11.position.z = 10;
 
 
 
