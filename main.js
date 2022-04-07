2a5f1df52658cff4b3b29751bd81ca9376865f30
MGibson7 <MGibsonASC@gmail.com>
2022-04-07 10:27:02 -0400

    Updates

--- main.js
+++ main.js
@@ -146,10 +146,10 @@ michael11.position.z = 10;
 //Add Event Listener
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
-var targetMesh
+
 function onMouseClick( event ) {
     raycaster.setFromCamera( mouse, camera );
-    var isIntersected = raycaster.intersectObject( event );
+    var isIntersected = raycaster.intersectObject( michael2 );
     if (isIntersected) {
         console.log('Mesh clicked!')
     }
@@ -158,6 +158,7 @@ function onMouseClick( event ) {
 
 
 
+window.addEventListener( 'mouseclick', onMouseClick, false );
 
 
 
@@ -173,7 +174,6 @@ function animate() {
   michael.rotation.z += 0.01;
  
   for (const item of array1){
-    onMouseClick(item);
     if (item.position.x >= -40){
       item.position.x += -0.1;
 
